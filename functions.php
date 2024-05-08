cc5a00bb5affbbe3764848184434e41b9c7490f1
Karla Crepaldi <163344451+KaCrepaldi@users.noreply.github.com>
2024-05-08 14:06:06 -0300

    Adicionar pasta Modulo PHP

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function boasVindas($nome) {
+      return $nome . ". Seja bem vindo!";
+}
+
+function media($a, $b){
+     $media =  ($a + $b) /2;
+     $msg = '';
+     
+     
+     if($media < 5) {
+        $msg = "reprovado";
+     } else {
+         $msg= "aprovado";
+
+     }
+     
+     return $msg;
+
+    }
