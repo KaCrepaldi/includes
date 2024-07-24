5d69d73ca41d9e679811e6b78d15ea1c0f21cb7b
Karla Crepaldi <163344451+KaCrepaldi@users.noreply.github.com>
2024-07-24 09:40:45 -0300

    Add files via upload

---
+++ listContacts.php
@@ -0,0 +1,23 @@
+<?php
+require_once('./includes/connection.php');
+
+
+$connection->select_db('contacts');
+
+$sql = "SELECT * from contact OR";
+$result = $connection->query($sql);
+
+if($connection->error) {
+    echo "erro no acesso a tabela: ". $connection->error;
+}
+
+$result -> fetch_all(MYSQLI_ASSOC);
+
+foreach($result as $row) {
+    printf("<li>%s %s - %s - %s <a href='./delete.php?
+    id=%s'>Excluir</a> </li>", $row["name"], $row["lastaname"], 
+    $row["email"], $row["phone"], $row['id']);
+
+}
+
+$connection->close();
